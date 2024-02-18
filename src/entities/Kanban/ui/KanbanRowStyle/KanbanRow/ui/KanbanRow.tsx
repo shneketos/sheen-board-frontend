a3dc5eee0f708f5f6df727f861e43227af00cb69
Sheen <shneketos@gmail.com>
2024-02-18 17:10:52 +0400

    added kanban row info modal

--- src/entities/Kanban/ui/KanbanRowStyle/KanbanRow/ui/KanbanRow.tsx
+++ src/entities/Kanban/ui/KanbanRowStyle/KanbanRow/ui/KanbanRow.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import styles from "./KanbanRow.module.scss";
 import { KanbanRowItem } from "../../KanbanRowItem";
 import MoreIcon from "shared/assets/icons/more.svg";
+import PencilIcon from "shared/assets/icons/pencil.svg";
+import TrashcanIcon from "shared/assets/icons/trashcan.svg";
 import { Button, ButtonTheme } from "shared/ui/Button/Button";
 import { KanbanRowAddItem } from "../../KanbanRowAddItem";
+import { InfoModal } from "shared/ui/InfoModal/InfoModal";
 
 export const KanbanRow = () => {
+    const [rowModalOpened, setRowModalOpened] = React.useState(false);
     return (
         <div className={styles.row}>
             <div className={styles.row_top}>
@@ -15,9 +19,56 @@ export const KanbanRow = () => {
                         <Button
                             className={styles.row_top_btn}
                             theme={ButtonTheme.CLEAR}
+                            onClick={() => setRowModalOpened(true)}
                         >
                             <MoreIcon width={16} height={16} />
                         </Button>
+                        {rowModalOpened && (
+                            <InfoModal className={styles.row_modal}>
+                                <div
+                                    className={styles.row_modal_wrapper}
+                                    onMouseLeave={() =>
+                                        setRowModalOpened(false)
+                                    }
+                                >
+                                    <Button
+                                        theme={ButtonTheme.CLEAR}
+                                        className={
+                                            styles.row_modal_wrapper_edit
+                                        }
+                                    >
+                                        <PencilIcon width={15} height={15} />
+                                        <span>Edit title</span>
+                                    </Button>
+                                    <Button
+                                        theme={ButtonTheme.CLEAR}
+                                        className={
+                                            styles.row_modal_wrapper_delete_cards
+                                        }
+                                    >
+                                        <TrashcanIcon
+                                            width={15}
+                                            height={15}
+                                            fill="red"
+                                        />
+                                        <span>Delete all cards</span>
+                                    </Button>
+                                    <Button
+                                        theme={ButtonTheme.CLEAR}
+                                        className={
+                                            styles.row_modal_wrapper_delete_row
+                                        }
+                                    >
+                                        <TrashcanIcon
+                                            width={15}
+                                            height={15}
+                                            fill="red"
+                                        />
+                                        <span>Delete row</span>
+                                    </Button>
+                                </div>
+                            </InfoModal>
+                        )}
                     </div>
                 </div>
             </div>
